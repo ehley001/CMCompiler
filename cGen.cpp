307748d35ae6e29eb465afa8d08a146a6eadaea3
ehley001 <ehley001@rangers.uwp.edu>
2018-04-17 11:04:59 -0500

    Added line numbers to the print statements, as it will not work with tm without those.

--- cGen.cpp
+++ cGen.cpp
@@ -11,22 +11,27 @@ void cGen::generate(unordered_map<int, SymTab*> local,  unordered_map<int, SymTa
 
     ofstream outfile (file);
 
+    int lineNum = 0;
 
     unordered_map<int, SymTab*>::iterator it = local.begin(); // initializing the map iterator
 
     // iterating through the function map
     while (it != local.end()){
 
-    outfile << "IN 4,0,0\n";
-        outfile << "ST 4, " << it->second->address << "(0)\n";
-        outfile << "LD 4, " << it->second->address << "(0)\n";
-        outfile << "OUT 4, 0,0\n";
+        outfile << lineNum << ": IN 4,0,0\n";
+            lineNum++;
+        outfile << lineNum <<": ST 4, " << it->second->address << "(0)\n";
+            lineNum++;
+        outfile << lineNum<< ": LD 4, " << it->second->address << "(0)\n";
+            lineNum++;
+        outfile << lineNum << ": OUT 4, 0,0\n";
+            lineNum++;
 
 
         it++; //moving forward in the map after everything has been printed for a function
     }
 
-    outfile << "HALT 0,0,0" << endl;
+    outfile << lineNum <<": HALT 0,0,0" << endl;
     outfile.close();
 
 }
